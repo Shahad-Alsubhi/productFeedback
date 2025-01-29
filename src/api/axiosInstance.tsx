45573dfdb68c1ae7958db7bdce0dd7d7e7ff28f0
Shahad Alsubhi <shahadalsubhi11@gmail.com>
2025-01-29 06:58:07 +0300

    update

--- src/api/axiosInstance.tsx
+++ src/api/axiosInstance.tsx
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: 
-  `http://localhost:3000`,
+  // `http://localhost:3000`,
+  'https://productfeedback-backend.onrender.com',
   headers: {
     'Content-Type': 'application/json',
     
